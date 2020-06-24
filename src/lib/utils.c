aaf4ac13629111d4ee2190a2b06d0f0d033bfcee
Apis-C <bastian.tessin@gmx.de>
2020-06-24 11:14:06 +0200

    Event-dispatcher

--- src/lib/utils.c
+++ src/lib/utils.c
@@ -6,14 +6,50 @@
  */
 #include "utils.h"
 
+/**
+ * @breif prints the given code to the board-leds.
+ * @param val A maximum of 15 is possible.
+ */
 void led_print(uint8_t val)
 {
-	LED_DDR &= ~( (1<<PD4) | (1<<PD5) | (1<<PD6) | (1<<PD7) );
+	LED_DDR &= ~(0x0F<<4);
 
-	if(val>0x10)
+	if(val>0x0F)
 	{
-		val=0x10;
+		val=0x0F;
 	}
 
 	LED_DDR |= val<<4;
+}
+
+#define QUEUE_LEN  10
+event_t event_queue[QUEUE_LEN];
+uint8_t read;
+uint8_t write;
+
+/**
+ * @brief Queues an event_t in a fifo-buffer
+ * @param ev event_t
+ */
+void queue_event(event_t ev)
+{
+	event_queue[write] = ev;
+	write = (write+1)%QUEUE_LEN;
+
+}
+
+/**
+ * @brief Reads and returns an event_t from an event_t-fifo-buffer.
+ * @return An unhandled event_t, EV_NOTHING else.
+ */
+event_t get_event(void)
+{
+	if (read!=write)
+	{
+		read = (read+1)%QUEUE_LEN;
+		return event_queue[read];
+	} else
+	{
+		return EV_NOTHING;
+	}
 }
--- src/lib/utils.h
+++ src/lib/utils.h
@@ -12,6 +12,31 @@
 
 #define LED_DDR DDRD
 
+typedef enum
+{ST_GET_USER_OPEN,
+	ST_GET_USER_CLOSE,
+	ST_MEAS_HUM,
+	ST_MEAS_TEMP,
+	ST_SLEEP,
+	ST_WAKEUP
+} state_t;
+
+typedef enum
+{EV_TEMP_THRESH_OPEN,
+	EV_TEMP_THRESH_CLOSE,
+	EV_RAIN,
+	EV_NO_RAIN,
+	EV_WIN_CLOSED,
+	EV_WIN_OPENED,
+	EV_WAKEUP,
+	EV_SLEEP,
+	EV_NOTHING
+} event_t;
+
+
+
 void led_print(uint8_t val);
+void queue_event(event_t ev);
+event_t get_event(void);
 
 #endif /* SRC_LIB_UTILS_H_ */
