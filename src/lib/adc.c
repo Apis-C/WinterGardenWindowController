dae8fc9efc0a47d67653cb19371f06a1b0dc596f
Apis-C <bastian.tessin@gmx.de>
2020-06-24 11:12:27 +0200

    ADC ready to use UNTESTED: No Hardware available

--- src/lib/adc.c
+++ src/lib/adc.c
@@ -7,17 +7,6 @@
 
 #include "adc.h"
 
-uint8_t volatile temp_available;
-uint8_t volatile temp_threshold;
-uint8_t volatile hum_available;
-
-uint16_t adc_filter[ADC_FILTER_SIZE];
-uint8_t volatile adc_isr_decider;
-enum
-{
-	NOTHING_ACTIVE, TEMP_ACTIVE, HUM_ACTIVE
-};
-
 /**
  * @brief Initializes the ADC.
  */
@@ -26,108 +15,36 @@ void adc_init(void)
 	ADC_DDR &= ~((1 << TEMP_SENSOR_PIN) | (1 << TEMP_THRESHOLD_PIN)
 			| (1 << HUM_SENSOR_PIN));
 	ADC_DDR |= ((1 << TEMP_POWERPIN) | (1 << HUM_POWERPIN));
-	ADCSRA |= ((1 << ADPS1) | (1 << ADPS0)); //Prescaler F_CPU / 8 => F_ADC = 125kHz
+	ADCSRA |= ((1 << ADPS1) | (1 << ADPS0)); //Prescaler F_CPU / 8 => F_ADC = 125kHz (F_CPU 1MHz)
 }
 
 static inline void adc_start(uint8_t muxpin)
 {
-	ADCSRA |= ((1 << ADEN) | (1 << ADSC)(1 << ADIE)); //enable ADC, start conversions, enable ADC-Interrupts
+	ADCSRA |= ((1 << ADEN) | (1 << ADSC)); //enable ADC, start conversions
 	ADMUX |= (1 << muxpin);
 }
 
-/**
- * @brief Starts a temperature measurement and clears temp_available.
- */
-void adc_temp_start_measurement(void)
+static inline adc_stop(uint8_t muxpin)
 {
-	temp_available = 0;
-	ADC_PORT |= (1 << TEMP_POWERPIN);				//turn on temperature sensor
-	adc_start(TEMP_SENSOR_PIN);
-	adc_isr_decider = TEMP_ACTIVE;
+	ADCSRA &= ~((1 << ADEN) | (1 << ADSC));
+	ADMUX &= ~(1 << muxpin);
 }
 
 /**
- * @brief Starts a humidity measurement.
+ * @brief Reads from ADC on the specified pin, filters the value and stores it in dst.
+ * @param pin ADC-Pin.
+ * @param dst uint16_t to store the value in.
  */
-void adc_hum_start_measurement(void)
+void analog_read_filtered(uint8_t pin, uint16_t *dst)
 {
-	hum_available = 0;
-	ADC_PORT |= (1 << HUM_POWERPIN);				//turn on humidity sensor
-	adc_start(HUM_SENSOR_PIN);
-	adc_isr_decider = HUM_ACTIVE;
-}
-
-/**
- * @brief Averages the ADC-measured values.
- * @param dst
- */
-static void adc_average(auto *dst)
-{
+	*dst=0;
+	adc_start(pin);
 	for (uint8_t i = 0; i < ADC_FILTER_SIZE; ++i)
 	{
-		*dst += adc_filter[i];
+		while (!(ADCSRA & (1 << ADIF)));
+		*dst += ADC;
 	}
-	*dst /= ADC_FILTER_SIZE; //TODO compute to real temperature
+	*dst /= ADC_FILTER_SIZE;
+	adc_stop(pin);
 }
 
-/**
- * @brief Returns the measured temperature and clears temp_available. If this is called while a conversion is running, nothing will be done.
- * @param dst The variable to write to.
- */
-void adc_get_temperature(int16_t *dst)
-{
-	if (temp_available)
-	{
-		temp_available = 0;
-		adc_average(dst);
-	}
-	//TODO compute to temperature
-}
-
-/**
- * @brief Returns the measured humidity and clears hum_available. If this is called while a conversion is running, nothing will be done.
- * @param dst The variable to write to.
- */
-void adc_get_humidity(uint8_t *dst)
-{
-	if (temp_available)
-	{
-		temp_available = 0;
-		adc_average(dst);
-	}
-	//TODO compute to humidity
-}
-
-ISR(ADC_vect) //add new value to the filter
-{
-	static uint8_t i = 0;
-	if (i >= ADC_FILTER_SIZE)	//filter full
-	{
-		ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0)); //close connection to ADC
-		ADCSRA &= ~((1 << ADEN) | (1 << ADSC)(1 << ADIE));		//shut down ADC
-		ADC_PORT &= ((1 << TEMP_POWERPIN) | (1 << HUM_POWERPIN));//turn off sensors
-		i = 0;
-
-		switch (adc_isr_decider)
-		{
-		case NOTHING_ACTIVE:
-			break;
-
-		case TEMP_ACTIVE:
-			temp_available = 1;
-			break;
-
-		case HUM_ACTIVE:
-			hum_available = 1;
-			break;
-
-		default:
-			break;
-		}
-	}
-	else //filter not full
-	{
-		adc_filter[i] = ADC;
-		++i;
-	}
-}
--- src/lib/adc.h
+++ src/lib/adc.h
@@ -22,16 +22,10 @@
 #define TEMP_THRESHOLD_PIN PC4		//POTI
 #define ADC_FILTER_SIZE 16
 
-extern volatile uint8_t temp_available;
-extern volatile uint8_t hum_available;
-extern volatile uint8_t temp_threshold;
 
 void adc_init(void);
-void adc_temp_start_measurement(void);
-void adc_hum_start_measurement(void);
-void adc_get_temperature(int16_t* dst);
-void adc_get_humidity(uint8_t* dst);
 
+void analog_read_filtered(uint8_t pin, uint16_t* dst);
 
 
 #endif /* LIB_TEMP_H_ */
